396ffc6ede46d293c0d59797f990e4e137dbd73e
Hoangthan1505 <andauphongvaankem@gmail.com>
2025-07-21 09:14:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Hiệu ứng tim bay lên trời
+document.addEventListener("DOMContentLoaded", function () {
+  setInterval(() => {
+    const heart = document.createElement("div");
+    heart.classList.add("heart-float");
+    heart.style.left = Math.random() * 100 + "vw";
+    document.body.appendChild(heart);
 
+    setTimeout(() => {
+      heart.remove();
+    }, 4000);
+  }, 300);
+});
